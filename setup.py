655250873e0f8e54575622bbe8e131a545f25d19
k0t3n <k0t3n.mail@gmail.com>
2021-05-24 11:03:12 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ from setuptools import setup
 setup(
     name='django_geogebra',
     version='0.0.1',
-    packages=['geogebra'],
-    url='',
+    packages=['django_geogebra'],
+    url='https://github.com/k0t3n/django_geogebra',
     license='MIT',
     author='Alexey Kotenko',
     author_email='alexey@kotenko.me',
-    description='Geogebra integration for Django'
+    description='Geogebra integration for Django',
+    install_requires=[
+        'django>=3.0'
+    ],
+    python_requires='>=3',
 )
